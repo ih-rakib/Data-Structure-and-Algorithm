0dc5a3010d7be1ed84dcbdec4d9ea400cd212cbd
Ikramul Hasan Rakib <hasanrakib3590@gmail.com>
2024-01-29 23:10:08 +0600

    another string problem

---
+++ String/Number of Changing Keys.cpp
@@ -0,0 +1,16 @@
+// Number of Changing Keys
+// https://leetcode.com/contest/weekly-contest-382/problems/number-of-changing-keys/
+
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int n = s.length(), ans = 0;
+        for(int i = 0; i < n; ++i){
+            if(isupper(s[i])) s[i] = tolower(s[i]);
+            if(i and s[i]!=s[i-1]) ans++;
+        }
+        return ans;
+    }
+};
+
+// // Author: Ikramul Hasan Rakib
